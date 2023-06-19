8391af25a969969454d0b76722dc470c5188ba89
Antonio <antonio18casta@aol.com>
2023-06-19 15:11:51 -0400

    commit tree 73d7d4bd344e127ac51bd3e9ec9d6a99ac9692a9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+# anagram is a word, phrase, or sentence formed from another by rearranging its letters
+
+### #filter / #select / #find_all
+# Use Case: When you want to access every element of an array, check if it matches some criteria,
+# and return a new array of all the values that match.
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        string.select { |w| w.split("").sort == word.split("").sort }
+    end
+end
